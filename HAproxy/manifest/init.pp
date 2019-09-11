eab36fea3c8e4d5bb6d0eb8a6ad01f568eb6cf67
Alexandr Yurchenko <iurchenko.a@gmail.com>
2019-09-11 20:55:07 +0300

    UPD

--- HAproxy/manifest/init.pp
+++ HAproxy/manifest/init.pp
@@ -20,3 +20,30 @@ class haproxy (
     notify  => Service['haproxy'],
   }
 }
+
+
+class haproxy::new (
+  $http          = true,
+  $frontend      = [],
+  $backend       = [],
+
+  $redis_node     = [],
+  $redis_pass     = '',
+  $redis_cluster  = '',
+  $redis_listen   = '6379',
+  ) {
+  package {'haproxy':
+    ensure => present }
+  zabbix::metadata { 'haproxy': }
+  service { ['haproxy']:
+    ensure => running,
+    enable   => true,
+    require  => Package['haproxy'],
+  }
+  file {'/etc/haproxy/haproxy_new.cfg':
+    ensure  => file,
+    content => template('haproxy.cfg.erb'),
+    require => Package['haproxy'],
+    notify  => Service['haproxy'],
+  }
+}
