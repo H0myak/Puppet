de02f73e96a5a4ff9912a2b8b2b9688bfac2ce81
Alexandr Yurchenko <iurchenko.a@gmail.com>
2020-04-09 21:48:48 +0300

    Upd

--- ceph/manifests/init.pp
+++ ceph/manifests/init.pp
@@ -1,9 +1,14 @@
 class ceph (
   $ceph_release = 'nautilus',
   $ceph_version = '14.2.8-0.el7',
-  $ceph_node    = true,
+  $ceph_node    = false,
   $ceph_deploy  = false,
-  $hw_node      = false,
+  $hv_node      = false,
+  $ceph_mon     = '',
+  $ceph_mon_ip  = '',
+  $ceph_mgr     = '',
+  $ceph_mds     = '',
+  $ceph_hosts   = '',
 
 ){
   class {'yum::ceph': ceph_release => $ceph_release }
@@ -11,16 +16,23 @@ class ceph (
     package { ['python-ceph-argparse', 'ceph-mgr', 'ceph', 'libcephfs2',
                'python-cephfs', 'ceph-common', 'ceph-selinux', 'ceph-osd',
                'ceph-mds', 'ceph-radosgw', 'ceph-base', 'ceph-mon']:
-    ensure => $ceph_version,
-    require => Class['yum::ceph'],
+      ensure => $ceph_version,
+      require => Class['yum::ceph'];
+    }
   }
   if $ceph_deploy {
     package { ['ceph-deploy']:
-    require => Class['yum::ceph'],
+      require => Class['yum::ceph'];
+    }
   }
-  if $hw_node {
+  if $hv_node {
     package { ['ceph-common']:
-    ensure => $ceph_version,
-    require => Class['yum::ceph'],
+      ensure => $ceph_version,
+      require => Class['yum::ceph'];
+    }
+    file { '/etc/ceph/ceph.conf':
+      content => template(ceph/hv_node.ceph.erb'),
+      require => Package['ceph-common'];
+    }
   }
 }
